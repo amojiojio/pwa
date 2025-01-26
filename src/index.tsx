2b120bc3e6f034ae9b23586f4472c803cf9a4373
seven-eleven <love_look@icloud.com>
2025-01-26 17:28:05 +0800

    init

---
+++ src/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './index.css';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+    <React.StrictMode>
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    </React.StrictMode>
+);
+
